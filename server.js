0824011c807e8b67428fa49ecca8ee72600ceca1
Jonathan Roth <jwayneroth@gmail.com>
2017-12-23 00:30:30 -0500

    linting and customizing logging

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
+//const winston = require('winston');
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const logger = require('./lib/core/logger').logger;
 const errorHandler = require('./lib/core/errorHandler');
 const config = require('./lib/core/config');
 const port = config.get('PORT');
-const createLoggers = require('namespaced-console-logger');
-const logger = createLoggers().get('namespace');
 const importerResource = require('./lib/importer/resource');
 const apiResource = require('./lib/api/resource');
 const usersResource = require('./lib/users/resource');
 const tokensResource = require('./lib/tokens/resource');
-
-const loggers = require('./lib/core/loggers');
 const models = require('./lib/core/models');
 const db = require('./lib/core/db.js');
 const app = express();
@@ -42,7 +40,6 @@ models.sync({
 	});
 })
 .catch(function(err) {
-	console.log(err);
 	throw err;
 });
 
@@ -54,7 +51,6 @@ if (config.get('NODE_ENV') !== 'test') {
 }
 
 app.use(cors());
-app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: '10mb' }));
 app.get('/_health', (req, res) => res.end('ok'));
 app.get('/favicon.ico', (req, res) => res.end());
@@ -69,5 +65,5 @@ app.get('*', (req, res) => {
 app.use(errorHandler);
 
 app.listen(port, () => {
-	logger.info(`listening on :${port}`);
+	logger.log('info', `listening on :${port}`);
 });
